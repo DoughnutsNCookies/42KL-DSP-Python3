6f9c010e67243fc834531161d60250002db48a56
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-09-21 05:45:04 +0000

    ex03

--- Python3/ex03/ft_calculator.py
+++ Python3/ex03/ft_calculator.py
@@ -6,23 +6,23 @@ class calculator:
 
     def __add__(self, num) -> None:
         """Calculator Add"""
-        self.my_list = list([x + num for x in self.my_list])
+        self.my_list = [x + num for x in self.my_list]
         print(self.my_list)
 
     def __mul__(self, object) -> None:
         """Calculator Mul"""
-        self.my_list = list([x * object for x in self.my_list])
+        self.my_list = [x * object for x in self.my_list]
         print(self.my_list)
 
     def __sub__(self, object) -> None:
         """Calculator Sub"""
-        self.my_list = list([x - object for x in self.my_list])
+        self.my_list = [x - object for x in self.my_list]
         print(self.my_list)
 
     def __truediv__(self, object) -> None:
         """Calculator Div"""
         try:
-            self.my_list = list([x / object for x in self.my_list])
+            self.my_list = [x / object for x in self.my_list]
             print(self.my_list)
         except ZeroDivisionError as err:
             print(err)
