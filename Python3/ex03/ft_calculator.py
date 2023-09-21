8958d167f86dcfcda90ba323620f1f19fe8b04e6
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-09-21 05:40:21 +0000

    ex03

--- Python3/ex03/ft_calculator.py
+++ Python3/ex03/ft_calculator.py
@@ -1,21 +1,26 @@
 class calculator:
     """Calculator Class"""
     def __init__(self, input_list) -> None:
+        """Calculator Init"""
         self.my_list = input_list
 
     def __add__(self, num) -> None:
+        """Calculator Add"""
         self.my_list = list([x + num for x in self.my_list])
         print(self.my_list)
 
     def __mul__(self, object) -> None:
+        """Calculator Mul"""
         self.my_list = list([x * object for x in self.my_list])
         print(self.my_list)
 
     def __sub__(self, object) -> None:
+        """Calculator Sub"""
         self.my_list = list([x - object for x in self.my_list])
         print(self.my_list)
 
     def __truediv__(self, object) -> None:
+        """Calculator Div"""
         try:
             self.my_list = list([x / object for x in self.my_list])
             print(self.my_list)
